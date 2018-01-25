ea945918ad0ae3257f0da68f3303b09dd5663efc
= <miguel.arguedas.mejias@gmail.com>
2018-01-25 13:04:37 -0600

    test.py add TreeData

--- demo/testapp/tests.py
+++ demo/testapp/tests.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.urls import reverse
 from django.contrib.auth.models import User, Permission, Group
 
+import datetime
 #TEST
 from django.test import TestCase
 from django.test import Client
@@ -13,118 +13,195 @@ from cruds_adminlte.utils import get_fields
 from cruds_adminlte import crud as crud_views
 
 #APPs
-from .models import Autor, Customer, Line,  
+from .models import Autor, Addresses, Customer, Invoice, Line
 
 class InsertData(TestCase):
-
-
-
-""" Filters test """
-class FilterViewTest(InsertData):
-    def setUp(self):   
-        self.user = User(
-            username='test', email='test@example.com', is_active=True,
-            is_staff=True, is_superuser=True,
-        )
-        self.user.set_password('test')
-        self.user.save()
-        
-        #insertar Customer
-        #insert Invoice
-        #insert Line
-        
-        
-        
-    def test_listview(self):
-        self.client.login(username='test', password='test')
-        url=reverse('testapp_invoice_list')
-        url+="?customer=&invoice_number=&initial-invoice_number=&date=&line=16&line=17"
-        response = self.client.get(url)
-        print (response.context['object_list'])    
-        self.assertQuerysetEqual(response.context['object_list'], [])
+    def setUp(self):
+        nobjects=4
+        for i in range(nobjects):  # add Autor-Address 
+             # autor 0   | autor 1   | autor 2   | autor 3
+             # address 0 | address 1 | address 2 | address 3
+             ao = Autor.object.create(name="author_name_%i"%i)
+             ado = Addresses.object.create(name="addresses_address_%i"%i,city="addresses_city_%i"%i,  autor=ao)
           
-
-
-
-""" Views test """
-class AuthUserViewTest(TestCase):
-
-    def setUp(self):
-        group_name = "My Test Group"
-        self.group = Group(name=group_name)
-        self.group.save()
-        
-        self.user = User(
-            username='test', email='test@example.com', is_active=True,
-            is_staff=True, is_superuser=True,
-        )
-        self.user.set_password('test')
-        self.user.save()
-
-        
-    def test_user_noLogin(self):
-        """ The User don't have been login """
-        response = self.client.get(reverse('auth_user_list'))
-        #self.assertContains(response.context,url)
-        self.assertEqual(response.status_code, 302 )
-        self.assertEqual(response.url,"/accounts/login/?next=/lte/auth/user/list")
-        
-        
-        
-    def test_user_list(self):
-        """ The user can load the users list,  when it's login """
-        self.client.login(username='test', password='test')
-        response = self.client.get(reverse('auth_user_list'))
-        #print (response)
-        self.assertEqual(response.status_code, 200 )
-        self.assertQuerysetEqual(response.context['object_list'],['<User: test>'])
-        
-        
-    def test_user_group_list(self): 
-        """ The user can lad group, when it's login """
-        self.client.login(username='test', password='test')
-        response = self.client.get(reverse('auth_user_groups_list'))
-        #print (response)
-        self.assertQuerysetEqual(response.context['object_list'], [])
-               
-   
-""" Admin user views test"""
-class AdminViewTestCase(TestCase):
-    def setUp(self):
-        group_name = "My Test Group"
-        self.group = Group(name=group_name)
-        self.group.save()
-        
-        self.user = User(
-            username='test', email='test@example.com', is_active=True,
-            is_staff=True, is_superuser=True,
-        )
-        self.user.set_password('test')
-        self.user.save()
-        self.client.login(username='test', password='test')
-        
-        
-    def tearDown(self):
-        self.client.logout()
-                    
-    def test_user_can_access(self):
-        """user in group should have access
-        """
-        self.user.groups.add(self.group)
-        self.user.save()
-        self.client.login(username='test', password='test')
-        response = self.client.get(reverse('auth_user_list'))
-        self.assertEqual(response.status_code, 200)      
-        
+          
+          
+        for i in range(nobjects):  # add Customer (Invoices < Lines)
+            co = Customer.object.create(name="customer_%i"%i)
             
-    def test_admin_not_broken(self):
-        """ user can use django admin """
-        response = self.client.get('/admin/')
-        self.assertContains(response, '/admin/password_change/')
-        self.assertNotContains(response, "You don't have permission to edit anything")
+            # customer 0 | customer 1 | customer 2 | customer 3
+            # invoice 0  | invoice 4  | invoice 8  | invoice 12
+            io = Invoice(customer=co,registered=True,sent=False,paid=False)  # Solo registrado
+            io.date =datetime.date.today()
+            io.save
+            # invoice 1  | invoice 5  | invoice 9  | invoice 13         
+            io = Invoice(customer=co,registered=True,sent=True,paid=False) # registrado y enviado
+            io.date =datetime.date.today()
+            io.save
+            # invoice 2  | invoice 6  | invoice 10 | invoice 14   
+            io = Invoice(customer=co,registered=False,sent=False,paid=False) # Sin procesar
+            io.date =datetime.date.today()
+            io.save
+            # invoice 3  | invoice 7  | invoice 11 | invoice 15
+            io = Invoice(customer=co,registered=True,sent=True,paid=True)  # completado
+            io.date =datetime.date.today()
+            io.save
+                    
 
-    def test_admin_auth_not_broken(self):
-        """ user can init auth """
-        response = self.client.get('/admin/auth/')
-        self.assertEqual(response.status_code, 200, response)
-       
+        for io in Invoice.object.all():
+             for i in range(nobjects):  
+                 lo=Line(invoice=io)
+                 lo.reference=io.customer.name+"_"+str(io.pk)+"_reference"+str(i)  #  customer_0 + 0 + _reference 0
+                 lo.concept=io.customer.name+"_"+str(io.pk)+"_concept"+str(i)   #  customer_0 + 0 + _concept 0
+                 lo.quantity="quantity_"+str(i)      #  quantity_0
+                 lo.unit=i
+                 lo.unit_price=i
+                 lo.amount=i
+                 lo.save
+            
+                 # ( customer 0) -------------------------------------------------------------------------------------------
+                 # invoice 0                                           | invoice 1
+                 #   * Line 0 i=0                                      |  *   Line 4    i=0
+                 #Line.reference=customer_0_0_reference_0              | Line.reference=customer_0_1_reference_0 
+                 #Line.concept=customer_0_0_concept_0                  | Line.concept=customer_0_1_concept_0
+                 #Line.concept=quantity_0                              | Line.concept=quantity_0           
+                 #   * Line 1 i=1                                      |   *  Line 5    i=1
+                 #Line.reference=customer_0_0_reference_1              | Line.reference=customer_0_5_reference_1           
+                 #Line.concept=customer_0_0_concept_1                  | Line.reference=customer_0_5_concept_1                   
+                 #Line.concept=quantity_1                              | Line.concept=quantity_1  
+                 #   * Line 2 i=2  ...                                 |   *  Line 6   i=2   ...
+                 #   * Line 3 i=3  ...                                 |   *  Line 7   i=3   ...              
+                 #----------                      ----------------------------------                      -----------------   
+                 # invoice 2                                           | invoice 3
+                 #   * Line 8   i=0                                    |  *   Line 12   i=0
+                 #Line.reference=customer_0_0_reference_0              | Line.reference=customer_0_1_reference_0 
+                 #Line.concept=customer_0_0_concept_0                  | Line.concept=customer_0_1_concept_0
+                 #Line.concept=quantity_0                              | Line.concept=quantity_0              
+                 #   *  Line 9  i=1                                    |   *  Line 13   i=1
+                 #Line.reference=customer_0_0_reference_1              | Line.reference=customer_0_1_reference_1           
+                 #Line.concept=customer_0_0_concept_1                  | Line.reference=customer_0_1_concept_1                   
+                 #Line.concept=quantity_1                              | Line.concept=quantity_1  
+                 #   * Line 10 i=2  ...                                |   *  Line 14  i=2   ...
+                 #   * Line 11 i=3  ...                                |   *  Line 15  i=3   ...                 
+                 # ( customer 1) -------------------------------------------------------------------------------------------        
+
+class   OListViewTest(InsertData):
+    
+    
+  
+# """ Filters test """
+# class FilterViewTest(InsertData):
+#     def setUp(self):   
+#         self.user = User(
+#             username='test', email='test@example.com', is_active=True,
+#             is_staff=True, is_superuser=True,
+#         )
+#         self.user.set_password('test')
+#         self.user.save()
+#         
+#         #insertar Customer
+#         #insert Invoice
+#         #insert Line
+#         
+#  """ Models query test """
+# class TestUtils(TestCase):
+# 
+#     def test_get_fields_order(self):
+#         res = get_fields(Autor, ('name',))
+#         self.assertEqual(list(res.keys())[0], 'name')
+# 
+#        
+#         customer_0 + 0 + _concept
+#     def test_listview(self):
+#         self.client.login(username='test', password='test')
+#         url=reverse('testapp_invoice_list')
+#         url+="?customer=&invoice_number=&initial-invoice_number=&date=&line=16&line=17"
+#         response = self.client.get(url)
+#         print (response.context['object_list'])    
+#         self.assertQuerysetEqual(response.context['object_list'], [])
+#           
+# 
+# 
+# 
+# """ Views test """
+# class AuthUserViewTest(TestCase):
+# 
+#     def setUp(self):
+#         group_name = "My Test Group"
+#         self.group = Group(name=group_name)
+#         self.group.save()
+#         
+#         self.user = User(
+#             username='test', email='test@example.com', is_active=True,
+#             is_staff=True, is_superuser=True,
+#         )
+#         self.user.set_password('test')
+#         self.user.save()
+# 
+#         
+#     def test_user_noLogin(self):
+#         """ The User don't have been login """
+#         response = self.client.get(reverse('auth_user_list'))
+#         #self.assertContains(response.context,url)
+#         self.assertEqual(response.status_code, 302 )
+#         self.assertEqual(response.url,"/accounts/login/?next=/lte/auth/user/list")
+#         
+#         
+#         
+#     def test_user_list(self):
+#         """ The user can load the users list,  when it's login """
+#         self.client.login(username='test', password='test')
+#         response = self.client.get(reverse('auth_user_list'))
+#         #print (response)
+#         self.assertEqual(response.status_code, 200 )
+#         self.assertQuerysetEqual(response.context['object_list'],['<User: test>'])
+#         
+#         
+#     def test_user_group_list(self): 
+#         """ The user can lad group, when it's login """
+#         self.client.login(username='test', password='test')
+#         response = self.client.get(reverse('auth_user_groups_list'))
+#         #print (response)
+#         self.assertQuerysetEqual(response.context['object_list'], [])
+#                
+#    
+# """ Admin user views test"""
+# class AdminViewTestCase(TestCase):
+#     def setUp(self):
+#         group_name = "My Test Group"
+#         self.group = Group(name=group_name)
+#         self.group.save()
+#         
+#         self.user = User(
+#             username='test', email='test@example.com', is_active=True,
+#             is_staff=True, is_superuser=True,
+#         )
+#         self.user.set_password('test')
+#         self.user.save()
+#         self.client.login(username='test', password='test')
+#         
+#         
+#     def tearDown(self):
+#         self.client.logout()
+#                     
+#     def test_user_can_access(self):
+#         """user in group should have access
+#         """
+#         self.user.groups.add(self.group)
+#         self.user.save()
+#         self.client.login(username='test', password='test')
+#         response = self.client.get(reverse('auth_user_list'))
+#         self.assertEqual(response.status_code, 200)      
+#         
+#             
+#     def test_admin_not_broken(self):
+#         """ user can use django admin """
+#         response = self.client.get('/admin/')
+#         self.assertContains(response, '/admin/password_change/')
+#         self.assertNotContains(response, "You don't have permission to edit anything")
+# 
+#     def test_admin_auth_not_broken(self):
+#         """ user can init auth """
+#         response = self.client.get('/admin/auth/')
+#         self.assertEqual(response.status_code, 200, response)
+#        
