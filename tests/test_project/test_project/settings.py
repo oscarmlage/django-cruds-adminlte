768b1d8cc2dcc3fad7c9b52b382a243d49cfe8b5
Marc Fargas <telenieko@telenieko.com>
2019-06-09 19:59:14 +0200

    More on testing (tox fixes)

--- tests/test_project/test_project/settings.py
+++ tests/test_project/test_project/settings.py
@@ -53,7 +53,7 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
