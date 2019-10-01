23a68039acd28edeffbcff78bc3d12bbe58d25c9
Marc Fargas <telenieko@telenieko.com>
2019-10-01 11:32:46 +0200

    Add a bit more testing

--- tests/test_template_loader.py
+++ tests/test_template_loader.py
@@ -9,9 +9,11 @@ def test_get_loaders():
     assert "block nav_links_ul" not in tpl_index.template.source
 
     tpl_base = loader.get_template('adminlte/base.html')
-    assert "tests/demo_project/demo/templates/adminlte/base.html" in tpl_base.origin.name
-    
+    assert "tests/demo_project/demo/templates/adminlte/base.html" in tpl_base.origin.name    
     base_content = tpl_base.render()
-    # The trick is that if this appears in the rendered content,
-    # everything went as planned.   
+    assert "fa-address-book-o" in base_content
+
+    tpl_base2 = loader.get_template('cruds/base.html')
+    assert "templates/cruds/base.html" in tpl_base2.origin.name    
+    base_content = tpl_base2.render()
     assert "fa-address-book-o" in base_content
